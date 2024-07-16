785ea31d45e80e04e8803070618d380ddc549af3
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-07-16 18:43:26 +0200

    feat: error page

--- src/screen/ErrorScreen.js
+++ src/screen/ErrorScreen.js
@@ -1,11 +1,64 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+import Lottie from 'react-lottie';
+import animationData from '../animation/not-found.json';
+import '../style/errore.scss';
+import backgroundImage from '../images/bg-error.jpg'; 
+import { Link } from 'react-router-dom';
 
 const ErrorScreen = () => {
   return (
-    <div>
-        <h1>Error Screen</h1>
-    </div>
-  )
-}
+    <Wrapper>
+      <BackgroundImage />
+      <ContentWrapper>
+        <div className='row align-items-center'>
+          <div className='col-12'>
+            <Lottie 
+              options={{
+                loop: true,
+                autoplay: true,
+                reverse: true,
+                animationData: animationData,
+                rendererSettings: {
+                  preserveAspectRatio: 'xMidYMid slice'
+                }
+              }}
+              width={350}
+              height={200}
+            />
+          </div>
+          <div className='col-12'>
+            <Link to="/" className='btn-return mx-4'>Ritorna alla Home</Link>
+          </div> 
+        </div>
+      </ContentWrapper>
+    </Wrapper>
+  );
+};
 
-export default ErrorScreen
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  text-align: center;
+  position: relative;
+`;
+
+const ContentWrapper = styled.div`
+  position: relative;
+  z-index: 1; 
+`;
+
+const BackgroundImage = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(${backgroundImage});
+  background-size: cover;
+  background-position: center;
+  filter: blur(4px); 
+`;
+
+export default ErrorScreen;
