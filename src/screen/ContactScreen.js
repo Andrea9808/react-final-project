f2cd267dbed7658f2842a04b1655081bfa9dfb51
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-07-17 11:40:58 +0200

    feat: form & integrazione di formspree

--- src/screen/ContactScreen.js
+++ src/screen/ContactScreen.js
@@ -1,11 +1,48 @@
-import React from 'react'
+import React from 'react';
+import '../style/contact_screen.scss';
+import Hero from '../components/Hero';
+import bg_contact from '../images/bg_contact.jpg';
 
 const ContactScreen = () => {
   return (
-    <div>
-        <h1>Contact Screen</h1>
-    </div>
-  )
-}
+    
+      <Hero img={bg_contact} disableOverlay>
+        <div className="contact-container d-flex flex-column align-items-center">
+          <div className='mt-5'>
+            <h1 className="text-white">Contattaci</h1>
+            <p className="subtitle">Hai bisogno di aiuto? Contattaci subito!</p>
+          </div>
+          <div className="form-container m-5">
+            <form className="form"
 
-export default ContactScreen
+                // action è l'URL del server che riceverà i dati del form di Formspree
+                action="https://formspree.io/f/xrbzbjzr"
+                method="POST"
+            >
+              <div className="form-group">
+                <label htmlFor="nome">Nome</label>
+                <input type="text" id="nome" name="nome" placeholder="Inserisci nome" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Inserisci email" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="telefono">Telefono</label>
+                <input type="tel" id="telefono" name="telefono" placeholder="Inserisci telefono" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="messaggio">Messaggio</label>
+                <textarea id="messaggio" name="messaggio" placeholder="Inserisci messaggio"></textarea>
+              </div>
+              <button className="form-submit-btn" type="submit">
+                Invia
+              </button>
+            </form>
+          </div>
+        </div>
+      </Hero>
+  );
+};
+
+export default ContactScreen;
