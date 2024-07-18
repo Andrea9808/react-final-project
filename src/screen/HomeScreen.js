d1d76eab419bee90aa6805b67133e79ba8949f04
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-07-18 18:30:47 +0200

    fix style

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -45,15 +45,16 @@ const HomeScreen = () => {
         <>
             <Hero>
                 <StyledContent>
-                    <div className='100-vh d-flex flex-column align-items-center justify-content-center text-white'>
+                    <div className='d-flex flex-column align-items-center justify-content-center text-white'>
                         <h1 className='mb-2'>GameMaster</h1>
                         <p>Benvenuto/a su GameMaster, il sito web di riferimento per tutti gli appassionati di videogiochi.</p>
                         <p>GameMaster è più di un semplice database: è la tua risorsa completa per esplorare, scoprire e immergerti nel vasto mondo dei videogiochi.</p>
                         <p>Con una vasta collezione di titoli e informazioni dettagliate, <span style={{ color: "green", fontWeight: "bold" }}>GameMaster</span> ti aiuta a trovare i giochi più recenti, a riscoprire i classici intramontabili e a seguire le ultime novità del settore.</p>
                     </div>
                     <Link to='/about' className='btn_home_hero'>Esplora</Link>
-                    <div className='m-5'>
+                    <div className='d-none d-md-block' >
                         <Lottie
+                         
                             options={{
                                 loop: true,
                                 autoplay: true,
@@ -62,12 +63,12 @@ const HomeScreen = () => {
                                     preserveAspectRatio: 'xMidYMid slice'
                                 }
                             }}
-                            width={350}
-                            height={200}
+                            width={250}
+                            height={150}
                         />
                     </div>
                 </StyledContent>
-            </Hero>
+              </Hero>
             <section>
                 <div className='search-bar'>
                     <div className='container'>
@@ -128,7 +129,7 @@ const HomeScreen = () => {
 };
 
 const StyledContent = styled.div`
-    height: 100vh;
+   height: 100vh;
     display: flex;
     flex-direction: column;
     align-items: center;
