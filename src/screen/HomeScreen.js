2a13d01ec17a201a8e4a511bf47dc2cd25793bdd
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-07-18 14:44:30 +0200

    input search from api + pagination

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,84 +1,145 @@
-import React, {useEffect, useState} from 'react'
-import { FaSearch } from 'react-icons/fa'
-import '../style/home_screen.scss'
-import styled from 'styled-components'
-import Lottie from 'react-lottie'
-import animationData from '../animation/gaming.json'
-import { Link } from 'react-router-dom'
-import Hero from '../components/Hero'
+import React, { useState } from 'react';
+import { FaSearch } from 'react-icons/fa';
+import styled from 'styled-components';
+import Lottie from 'react-lottie';
+import { Link } from 'react-router-dom';
+import Hero from '../components/Hero';
+import useFetch from '../useFetch';
+import '../style/home_screen.scss';
+import animationData from '../animation/gaming.json';
 
 const HomeScreen = () => {
+    const { input, setInput, games, isLoading, isError, handleSearch, count } = useFetch();
+    const [currentPage, setCurrentPage] = useState(1);
 
-  const [input, setInput] = useState("Assassin's Creed")
+    // Numero di giochi da visualizzare per pagina
+    const itemsPerPage = 4; 
 
-  return (
-    <>
-        <Hero>
-          <StyledContent>
-            <div className='100-vh d-flex flex-column align-items-center justify-content-center text-white'>
-              <h1 className='mb-2'>GameMaster</h1>
-              <p>Benvenuto/a su GameMaster, il sito web di riferimento per tutti gli appassionati di videogiochi.</p>
-              <p>GameMaster è più di un semplice database: è la tua risorsa completa per esplorare, scoprire e immergerti nel vasto mondo dei videogiochi.</p>
-              <p>Con una vasta collezione di titoli e informazioni dettagliate, <span style={{
-                color: "green",
-                fontWeight: "bold"
-              }}>GameMaster</span>  ti aiuta a trovare i giochi più recenti, a riscoprire i classici intramontabili e a seguire le ultime novità del settore.</p>
-            </div>
-            <Link to='/about' className='btn_home_hero'>Esplora</Link>
-            <div className='m-5'>
-              <Lottie 
-                options={{
-                  loop: true,
-                  autoplay: true,
-                  animationData: animationData,
-                  rendererSettings: {
-                    preserveAspectRatio: 'xMidYMid slice'
-                  }
-                }}
-                width={350}
-                height={200}
-              />
-            </div>
-            
-          </StyledContent>
-         
-        </Hero>
-        <section>
-           <div className='search-bar'>
-                <div className='container'>
-                  <form>
-                    <label htmlFor='game'>
-                       <h4>Cerca il tuo videogame</h4>
-                    </label>
-                    <div className='d-flex '>
-                      <input
-                        type='text'
-                        id ='game'
-                        className='input'
-                        placeholder={input}
-                        value={input}
-                        onChange={(e) => setInput(e.target.value)}
-                      />
-                      <button className='btn_search'>
-                        <FaSearch className='icon'/>
-                      </button>
+    
+    const indexOfLastGame = currentPage * itemsPerPage;
+    const indexOfFirstGame = indexOfLastGame - itemsPerPage;
+    const currentGames = games.slice(indexOfFirstGame, indexOfLastGame);
+
+    // Cambia pagina
+    const paginate = pageNumber => setCurrentPage(pageNumber);
+
+    return (
+        <>
+            <Hero>
+                <StyledContent>
+                    <div className='100-vh d-flex flex-column align-items-center justify-content-center text-white'>
+                        <h1 className='mb-2'>GameMaster</h1>
+                        <p>Benvenuto/a su GameMaster, il sito web di riferimento per tutti gli appassionati di videogiochi.</p>
+                        <p>GameMaster è più di un semplice database: è la tua risorsa completa per esplorare, scoprire e immergerti nel vasto mondo dei videogiochi.</p>
+                        <p>Con una vasta collezione di titoli e informazioni dettagliate, <span style={{ color: "green", fontWeight: "bold" }}>GameMaster</span> ti aiuta a trovare i giochi più recenti, a riscoprire i classici intramontabili e a seguire le ultime novità del settore.</p>
                     </div>
-                  </form>
-                  <p className='result'>3 risultati</p>
+                    <Link to='/about' className='btn_home_hero'>Esplora</Link>
+                    <div className='m-5'>
+                        <Lottie
+                            options={{
+                                loop: true,
+                                autoplay: true,
+                                animationData: animationData,
+                                rendererSettings: {
+                                    preserveAspectRatio: 'xMidYMid slice'
+                                }
+                            }}
+                            width={350}
+                            height={200}
+                        />
+                    </div>
+                </StyledContent>
+            </Hero>
+            <section>
+                <div className='search-bar'>
+                    <div className='container'>
+                        <form onSubmit={(event) => handleSearch(event, input)}>
+                            <label htmlFor='game'>
+                                <h4>Cerca il tuo videogioco</h4>
+                            </label>
+                            <div className='d-flex'>
+                                <input
+                                    type='text'
+                                    id='game'
+                                    className='input'
+                                    placeholder="Inserisci il nome del gioco"
+                                    value={input}
+                                    onChange={(e) => setInput(e.target.value)}
+                                />
+                                <button type='submit' className='btn_search'>
+                                    <FaSearch className='icon' />
+                                </button>
+                            </div>
+                        </form>
+                        {isLoading && <p>Caricamento...</p>}
+                        {isError && <p>Si è verificato un errore durante il caricamento dei dati.</p>}
+                        <p className='result'>{count} risultati</p>
+                    </div>
                 </div>
-           </div>
-        </section>
-    </>
-  )
-}
+                <div className='container'>
+                    <div className='row mb-5'>
+                        {currentGames.map(game => (
+                            <div className='col-12 col-md-6 col-lg-3 mb-4' key={game.id}>
+                                <div className='card'>
+                                {game.cover ? (
+                                    <img
+                                        src={`https:${game.cover.url.replace('t_thumb', 't_cover_big')}`}
+                                        className='card-img-top'
+                                        alt={`${game.name} cover`}
+                                    />
+                                    ) : (
+                                        <div className='text-white'>Immagine non disponibile</div>
+                                    )}
+                                    <div className='card-body'>
+                                        <h5 className='card-title'>{game.name}</h5>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                    {/* Impaginazione */}
+                    <Pagination className='m-5'>
+                        {Array.from({ length: Math.ceil(games.length / itemsPerPage) }, (_, index) => (
+                            <PageNumber key={index} onClick={() => paginate(index + 1)}>{index + 1}</PageNumber>
+                        ))}
+                    </Pagination>
+                </div>
+            </section>
+        </>
+    );
+};
 
 const StyledContent = styled.div`
-  height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  text-align: center;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
 `;
 
-export default HomeScreen
+const Pagination = styled.div`
+   display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(50px, 1fr)); 
+    gap: 10px;
+    justify-items: center;
+    margin-top: 20px;
+`;
+
+const PageNumber = styled.div`
+    cursor: pointer;
+    padding: 8px 16px;
+    margin: 0 5px;
+    background-color: white;
+    color: black;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    transition: 0.3s ease;
+
+    &:hover {
+        background-color: black;
+        color: white;
+    }
+`;
+
+export default HomeScreen;
--- src/useFetch.js
+++ src/useFetch.js
@@ -1,15 +1,49 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
-const url = "https://api.igdb.com/v4/games"
-const client_id = 'akhchja2t72t0afrjp5tot6pxx2p8n'
-const access_token = '0ze23d44k9j6fir5sv59a192hsdssm'
-const type_token = 'Bearer'
+import { useState } from 'react';
+import axios from 'axios';
 
-
-const useFetch = (type = false, query) => {
-    const url = type ? `https://api.igdb.com/v4/${type}` : "https://api.igdb.com/v4/games"
-    const [isLoading, setIsLoading] = useState(true);
-    const [data, setData] = useState([]);
-    const [count, setCount] = useState(0);
+const useFetch = () => {
+    const [input, setInput] = useState('');
+    const [count, setCount] = useState(0); // Inizializza count a 0
+    const [games, setGames] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
     const [isError, setIsError] = useState(false);
-}
+
+    const apiUrl = "https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4/games";
+
+    const handleSearch = async (event, searchInput) => {
+        event.preventDefault();
+    
+        setIsLoading(true);
+        setIsError(false);
+    
+        try {
+            const response = await axios({
+                method: 'GET',
+                url: apiUrl,
+                headers: {
+                    'Client-ID': 'akhchja2t72t0afrjp5tot6pxx2p8n',
+                    'Authorization': 'Bearer 72w8kzjf4dif9h14yhmwolg1cmrmgb'
+                },
+                params: {
+                    fields: 'name, cover.url, summary', 
+                    search: searchInput,
+                    limit: 40,
+                    'locale': 'IT-it' 
+                }
+            });
+    
+            setGames(response.data);
+            setCount(response.data.length);
+            console.log('Data from IGDB:', response.data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            setIsError(true);
+        }
+    
+        setIsLoading(false);
+    };
+
+    return { input, setInput, games, isLoading, isError, handleSearch, count };
+};
+
+export default useFetch;
