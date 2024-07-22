2e3443bd76e9c70c23324dd9487420bf152a5e9b
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-07-22 12:11:13 +0200

    insert log per netlify

--- src/netlify/functions/proxy.js
+++ src/netlify/functions/proxy.js
@@ -6,6 +6,8 @@ exports.handler = async function (event, context) {
     const CLIENT_ID = process.env.CLIENT_ID;
     const AUTHORIZATION = `Bearer ${process.env.AUTHORIZATION}`;
 
+    console.log('Request Body:', event.body);  // Log per netlify
+
     try {
         const response = await axios({
             method: 'POST',
@@ -18,15 +20,17 @@ exports.handler = async function (event, context) {
             data: event.body
         });
 
+        console.log('Response Data:', response.data);  // Log per netlify
+
         return {
             statusCode: 200,
             body: JSON.stringify(response.data),
         };
     } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error('Error fetching data:', error.message);  //Log per netlify
         return {
             statusCode: 500,
-            body: JSON.stringify({ message: 'Error fetching data' }),
+            body: JSON.stringify({ message: 'Error fetching data', error: error.message }),
         };
     }
 };
